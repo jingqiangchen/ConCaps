0e5dc43a8b83be394173ed0c3090a9b3caf52c9d
jingqiangchen <cjq@njupt.edu.cn>
2023-07-28 16:16:46 +0800

    Delete demo directory

--- demo/backend/tat/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TatConfig(AppConfig):
-    name = 'tat'
--- demo/backend/tat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from tat import views
-
-urlpatterns = [
-    path('scrape/', views.get_image_urls),
-    path('caption/', views.post_caption),
-]
